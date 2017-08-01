4b4e841e3870343de5740baf481fc99749300f70
Tatchagon Koonkoei <o_k_t@hotmail.com>
2017-08-01 18:08:51 +0700

    Modify the decrement() Method

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void decrement(View view){
         int quantity = 2;
-        quantity = 1;
+        quantity = quantity - 1;
         display(quantity);
     }
 
