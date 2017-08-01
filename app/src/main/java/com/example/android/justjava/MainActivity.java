f4b066732131ed7a10bbe6a2c6dbe5020fd7c7a2
Tatchagon Koonkoei <o_k_t@hotmail.com>
2017-08-01 17:50:09 +0700

    Update a Variable in the App

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -21,7 +21,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the plus button is clicked.
      */
     public void increment(View view){
-        int quantity = 3;
+        int quantity = 2;
+        quantity = 3;
         display(quantity);
     }
 
@@ -29,7 +30,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the minus button is clicked.
      */
     public void decrement(View view){
-        int quantity = 1;
+        int quantity = 2;
+        quantity = 1;
         display(quantity);
     }
 
