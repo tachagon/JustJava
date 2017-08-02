b5516ca9a57ff8b92c1d1b277ee4b8c1b9f641bd
Tatchagon Koonkoei <o_k_t@hotmail.com>
2017-08-02 22:50:27 +0700

    Update the String Variable

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -40,7 +40,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceMessage = "Free";
+        String priceMessage = "Total: $" + (quantity * 5);
+        priceMessage = priceMessage + "\nThank you!";
         displayMessage(priceMessage);
     }
 
